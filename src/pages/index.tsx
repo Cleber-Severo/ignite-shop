f84ef9f4f205f2c792699678728aff5ce1e53a4f
Cleber Severo <clebersevero1998@gmail.com>
2025-12-21 12:19:04 -0300

    feat: add carousel layout with products and hover effects

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,36 @@
-import { styled } from "../styles";
-
-export const Button = styled("button", {
-  backgroundColor: "$green500",
-  color: "white",
-  borderRadius: 4,
-  border: 0,
-  padding: "4px 8px",
-
-  "&:hover": {
-    filter: "brightness(0.8)",
-  },
-});
+import Image from "next/image";
+import { HomeContainer, Product } from "../styles/pages/home";
+import camiseta1 from "../assets/camisetas/1.png";
+import camiseta2 from "../assets/camisetas/2.png";
+import camiseta3 from "../assets/camisetas/3.png";
 
 export default function Home() {
   return (
-    <div>
-      <h1>hello world</h1>
-      <Button>Click me</Button>
-    </div>
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} width={520} height={480} alt="Camiseta 1" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta2} width={520} height={480} alt="Camiseta 1" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product>
+        <Image src={camiseta3} width={520} height={480} alt="Camiseta 1" />
+
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
